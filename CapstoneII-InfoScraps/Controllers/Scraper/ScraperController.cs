a3de2b4fb88116e0bed4d059d0def6f832fb0296
m-somerville33 <masonsomerville33@gmail.con>
2026-01-19 16:19:33 -0700

    Added ScraperController and Scraper View

---
+++ CapstoneII-InfoScraps/Controllers/Scraper/ScraperController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CapstoneII_InfoScraps.Controllers.Scraper
+{
+    public class ScraperController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
