8dc207f639d6404258c04a8f70ee09bafd32bbd2
m-somerville33 <masonsomerville33@gmail.con>
2026-01-19 15:41:37 -0700

    Forgot to stage and commit the entire project.

---
+++ CapstoneII-InfoScraps/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CapstoneII_InfoScraps.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
