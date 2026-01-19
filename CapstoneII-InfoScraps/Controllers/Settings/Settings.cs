7d38549ba9dc607abe0a9f5d920ff752ad3c0ff6
m-somerville33 <masonsomerville33@gmail.con>
2026-01-19 16:18:03 -0700

    Added SettingsController and Settings view

---
+++ CapstoneII-InfoScraps/Controllers/Settings/Settings.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CapstoneII_InfoScraps.Controllers.Settings
+{
+    public class Settings : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
