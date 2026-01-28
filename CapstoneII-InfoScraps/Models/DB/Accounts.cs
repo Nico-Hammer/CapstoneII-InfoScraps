1950fc6730748841922f7414de21f626a5879827
Nico-Hammer <nico.hammer@rdpolytech.ca>
2026-01-27 19:22:22 -0700

    added entity fields and basic constraints

--- CapstoneII-InfoScraps/Models/DB/Accounts.cs
+++ CapstoneII-InfoScraps/Models/DB/Accounts.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CapstoneII_InfoScraps.Models.DB;
 
 public class Accounts
 {
     // pk id
+    [Required]
+    public int Id { get; set; }
     // fk user
+    public ICollection<Users> Users { get; set; }
     // fk email templates
+    public ICollection<EmailTemplates> Email_Templates { get; set; }
     // fk scraped data
+    public ICollection<ScrapedData> Scraped_Data { get; set; }
 }
--- CapstoneII-InfoScraps/Models/DB/ScrapedData.cs
+++ CapstoneII-InfoScraps/Models/DB/ScrapedData.cs
@@ -1,12 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CapstoneII_InfoScraps.Models.DB;
 
 public class ScrapedData
 {
    // pk id
+   [Required]
+   public int Id { get; set; }
    // fk accountid
+   public int AccountId { get; set; }
+   public Accounts Account { get; set; } = null!;
    // scraped name
+   public string? Scraped_Name { get; set; }
    // scraped email
+   public string? Scraped_Email { get; set; }
    // scraped phone number
+   public string? Scraped_Phone{ get; set; }
    // time of scrape
+   [Required]
+   public DateTime Date_Of_Scrape { get; set; }
    // website scraped from
+   [Required]
+   public string Website { get; set; }
 }
