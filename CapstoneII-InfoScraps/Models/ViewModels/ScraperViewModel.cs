b5ea169ca06d7521238cec7fd99d7f72ed67a217
m-somerville33 <masonsomerville33@gmail.con>
2026-02-04 03:21:50 -0700

    Added ScraperViewModel for scraper input validation

---
+++ CapstoneII-InfoScraps/Models/ViewModels/ScraperViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CapstoneII_InfoScraps.Models.ViewModels
+{
+    public class ScraperViewModel
+    {
+        [Required(ErrorMessage = "Website URL is required")]
+        [Url(ErrorMessage = "Please enter a valid website URL")]
+        public string WebsiteUrl { get; set; }
+
+        [Required(ErrorMessage = "Email regex is required")]
+        public string EmailRegex { get; set; }
+    }
+}
