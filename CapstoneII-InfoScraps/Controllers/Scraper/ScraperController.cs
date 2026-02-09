e08f73e6e77749ea64f838123a6469fb3a552391
m-somerville33 <masonsomerville33@gmail.con>
2026-02-09 01:18:26 -0700

    Updated ScraperController and ScraperViewModel to integrate scraper API and display lead data

--- CapstoneII-InfoScraps/Controllers/Scraper/ScraperController.cs
+++ CapstoneII-InfoScraps/Controllers/Scraper/ScraperController.cs
@@ -1,12 +1,20 @@
 ﻿using CapstoneII_InfoScraps.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace CapstoneII_InfoScraps.Controllers.Scraper
 {
     public class ScraperController : Controller
     {
         private const string URLRegexPattern = @"^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+?$";
-        
+
+        // URL of the Python based scraper API
+        private const string ScraperAPIURL = "https://localhost:"; // Fill the rest in once we know what the endpoint will be
+
         [HttpGet]
         public IActionResult Index()
         {
@@ -14,7 +22,7 @@ namespace CapstoneII_InfoScraps.Controllers.Scraper
         }
 
         [HttpPost]
-        public IActionResult Index(ScraperViewModel model)
+        public async Task<IActionResult> Index(ScraperViewModel model)
         {
             // Check required fields and URL format
             if (!ModelState.IsValid)
@@ -29,6 +37,35 @@ namespace CapstoneII_InfoScraps.Controllers.Scraper
                 return View(model);
             }
 
+            // Create an HTTP client to talk to the Python scraper API
+            using var client = new HttpClient();
+
+            try
+            {
+                // Send the validated URL to the scraper API
+                var response = await client.PostAsJsonAsync(ScraperAPIURL, new { url = model.WebsiteUrl });
+
+                // Fail if the API returns an error status
+                response.EnsureSuccessStatusCode();
+
+                // Read scraped data returned by the API
+                var results = await response.Content.ReadFromJsonAsync<ScraperViewModel>();
+
+                // Safely copy scraped results into the view model
+                model.Emails = results?.Emails ?? new List<string>();
+                model.Names = results?.Names ?? new List<string>();
+
+                // Show a message if nothing was found
+                if (!model.Emails.Any() && !model.Names.Any())
+                {
+                    model.ErrorMessage = "No contact information was found on this website.";
+                }
+            }
+            catch
+            {
+                // Handle API or network failures
+                model.ErrorMessage = "Unable to connect to the scraping service. Please try again later.";
+            }
 
             return View(model);
         }
--- CapstoneII-InfoScraps/Models/ViewModels/ScraperViewModel.cs
+++ CapstoneII-InfoScraps/Models/ViewModels/ScraperViewModel.cs
@@ -8,5 +8,11 @@ namespace CapstoneII_InfoScraps.Models.ViewModels
         [Url(ErrorMessage = "Please enter a valid website URL")]
         public string WebsiteUrl { get; set; }
 
+        // Scraped data
+        public List<string> Names { get; set; } = new();
+        public List<string> Emails { get; set; } = new();
+
+        // Feedback to the user
+        public string ErrorMessage { get; set; }
     }
 }
