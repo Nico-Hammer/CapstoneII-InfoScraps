b9c0a95daa72deca0660eea95110ad8f9f851b05
Nico-Hammer <nico.hammer@rdpolytech.ca>
2026-02-14 09:19:49 -0700

    added proper validations on the user model

--- CapstoneII-InfoScraps/Models/DB/Users.cs
+++ CapstoneII-InfoScraps/Models/DB/Users.cs
@@ -1,5 +1,4 @@
 using System.ComponentModel.DataAnnotations;
-using System.Runtime.InteropServices;
 
 namespace CapstoneII_InfoScraps.Models.DB;
 
@@ -12,14 +11,22 @@ public class User
     public int AccountId { get; set; }
     public Account Account { get; set; } = null!;
     // username
-    [Required]
+    [Required(ErrorMessage = "Enter a username")]
     public string Username { get; set; }
     // email address
-    [Required]
+    [Required(ErrorMessage = "Enter an email address")]
+    [DataType(DataType.EmailAddress)] 
+    [EmailAddress(ErrorMessage = "Enter a valid email address")]
     public string Email { get; set; }
     // phone number
+    [DataType(DataType.PhoneNumber)]
+    [Phone(ErrorMessage = "Enter  a valid phone number")]
     public string? Phone_Number { get; set; }
     // password
-    [Required]
+    // regex from https://full-time.learnhowtoprogram.com/c-and-net/authentication-with-identity/authentication-with-identity-user-registration-viewmodel-validation-and-views
+    [Required(ErrorMessage = "Enter a password")]
+    [DataType(DataType.Password)]
+    [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", 
+        ErrorMessage = "Your password must contain at least 8 characters, a capital letter, a lowercase letter, a number, and a special character.")]
     public string Password { get; set; }
 }
